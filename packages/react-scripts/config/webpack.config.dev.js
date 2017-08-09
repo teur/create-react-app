686eb7be1e410b965bca2f13737b18618555a65d
Denis Golovnev <dgolovnev@qulix.com>
2017-08-09 16:31:50 +0300

    less support

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -65,6 +65,8 @@ module.exports = {
     // changing JS code would still trigger a refresh.
   ],
   output: {
+    // use library
+    library: 'ReactApp',
     // Next line is not used in dev but WebpackDevServer crashes without it:
     path: paths.appBuild,
     // Add /* filename */ comments to generated require()s in the output.
@@ -188,7 +190,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(css|less)$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -216,6 +218,9 @@ module.exports = {
                     }),
                   ],
                 },
+              },
+              {
+                loader: 'less-loader'
               },
             ],
           },
