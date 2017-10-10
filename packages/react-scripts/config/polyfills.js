7aaaf93e13ba1049e1ea855b2aa7547fe940a281
Denis Golovnev <dgolovnev@qulix.com>
2017-10-10 10:10:31 +0300

    less support

--- packages/react-scripts/config/polyfills.js
+++ packages/react-scripts/config/polyfills.js
@@ -13,7 +13,8 @@ if (typeof Promise === 'undefined') {
   // inconsistent state due to an error, but it gets swallowed by a Promise,
   // and the user has no idea what causes React's erratic future behavior.
   require('promise/lib/rejection-tracking').enable();
-  window.Promise = require('promise/lib/es6-extensions.js');
+  // for SSR
+  (typeof window !== 'undefined' ? window : global).Promise = require('promise/lib/es6-extensions.js');
 }
 
 // fetch() polyfill for making API calls.
